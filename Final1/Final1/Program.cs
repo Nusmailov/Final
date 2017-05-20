93c6d4fe963acb8dcf1455803e9273892cbb76c5
Nurzhigit <nusmailov@gmail.com>
2017-05-20 09:39:13 +0600

    20

---
+++ Final1/Final1/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Final1
+{
+    class Program
+    {
+        static bool check(int x)
+        {
+            for (int i = 3; i < x; i++)
+            {
+                if (i == 2 && i == 1)
+                {
+                    return true;
+                }
+                else if (x % i == 0)
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        static void Main(string[] args)
+        {
+            DirectoryInfo Finfo = new DirectoryInfo(@"G:\.Trash-1000\KBTU\2semestr\Final\Final1\Final1\bin\Debug");
+            int cnt = 0;
+           // string s = Console.ReadLine();
+            for (int i = 0; i < Finfo.GetFileSystemInfos().Length; i++)
+            {
+                if(Finfo.GetFileSystemInfos()[i].Name.Contains(".txt"))
+                {
+                    Console.Write(Finfo.GetFileSystemInfos()[i].Name);
+                    string s = Finfo.GetFileSystemInfos()[i].Name;
+                    StreamReader sr = new StreamReader(s);
+                    string p = sr.ReadLine();
+                    string[] t = p.Split();
+                    int[] arr = new int[t.Length];
+                    for (int j = 0; j < t.Length; j++)
+                    {
+                        arr[j] = int.Parse(t[j]);
+                        if(check(arr[j]))
+                        {
+                        Console.Write(" "+arr[j] + " ");
+                        }
+                    }
+                    Console.WriteLine();
+                        cnt++;
+                }
+            }
+            if (cnt == 0)
+            {
+                Console.WriteLine("Not Found");
+            }
+            Console.ReadKey();
+            //Console.WriteLine);
+
+        }
+    }
+}
