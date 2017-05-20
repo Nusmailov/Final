5fb7e8ec73b1e864b9ca6a96bfd9a77beb1b2fe5
Nurzhigit <nusmailov@gmail.com>
2017-05-20 11:11:51 +0600

    11:10

--- Final3/Final3/Form1.Designer.cs
+++ Final3/Final3/Form1.Designer.cs
@@ -47,9 +47,10 @@
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(-15, 0);
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(-35, -14);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(901, 345);
+            this.pictureBox1.Size = new System.Drawing.Size(936, 364);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
@@ -57,17 +58,19 @@
             // 
             // button1
             // 
+            this.button1.BackColor = System.Drawing.Color.White;
             this.button1.Location = new System.Drawing.Point(0, 0);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 1;
             this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.White;
             this.label1.Location = new System.Drawing.Point(366, 46);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(19, 13);
@@ -78,6 +81,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.White;
             this.label2.Location = new System.Drawing.Point(391, 58);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(13, 13);
@@ -88,6 +92,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.White;
             this.label3.Location = new System.Drawing.Point(406, 68);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(13, 13);
@@ -97,6 +102,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.White;
             this.label4.Location = new System.Drawing.Point(421, 86);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(13, 13);
@@ -106,51 +112,59 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(344, 114);
+            this.label5.BackColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(406, 99);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(13, 13);
             this.label5.TabIndex = 12;
-            this.label5.Text = "7";
+            this.label5.Text = "4";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(366, 134);
+            this.label6.BackColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(391, 125);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(13, 13);
             this.label6.TabIndex = 7;
-            this.label6.Text = "6";
+            this.label6.Text = "5";
             // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(391, 114);
+            this.label8.BackColor = System.Drawing.Color.White;
+            this.label8.Location = new System.Drawing.Point(344, 125);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(13, 13);
-            this.label8.TabIndex = 8;
-            this.label8.Text = "5";
+            this.label8.TabIndex = 7;
+            this.label8.Text = "7";
+            this.label8.Click += new System.EventHandler(this.label8_Click);
             // 
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(406, 99);
+            this.label9.BackColor = System.Drawing.Color.White;
+            this.label9.Location = new System.Drawing.Point(331, 99);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(13, 13);
-            this.label9.TabIndex = 9;
-            this.label9.Text = "4";
+            this.label9.TabIndex = 8;
+            this.label9.Text = "8";
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(325, 99);
+            this.label7.BackColor = System.Drawing.Color.White;
+            this.label7.Location = new System.Drawing.Point(366, 142);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(13, 13);
             this.label7.TabIndex = 10;
-            this.label7.Text = "8";
+            this.label7.Text = "6";
             // 
             // label10
             // 
             this.label10.AutoSize = true;
+            this.label10.BackColor = System.Drawing.Color.White;
             this.label10.Location = new System.Drawing.Point(310, 86);
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(13, 13);
@@ -160,6 +174,7 @@
             // label11
             // 
             this.label11.AutoSize = true;
+            this.label11.BackColor = System.Drawing.Color.White;
             this.label11.Location = new System.Drawing.Point(325, 68);
             this.label11.Name = "label11";
             this.label11.Size = new System.Drawing.Size(19, 13);
@@ -169,6 +184,7 @@
             // label12
             // 
             this.label12.AutoSize = true;
+            this.label12.BackColor = System.Drawing.Color.White;
             this.label12.Location = new System.Drawing.Point(344, 55);
             this.label12.Name = "label12";
             this.label12.Size = new System.Drawing.Size(19, 13);
--- Final3/Final3/Form1.cs
+++ Final3/Final3/Form1.cs
@@ -27,13 +27,197 @@ namespace Final3
             if (x == 0)
             {
                 label1.BackColor = Color.Red;
+                label2.BackColor = Color.White;
+                label3.BackColor = Color.White;
+                label4.BackColor = Color.White;
+                label5.BackColor = Color.White;
+                label6.BackColor = Color.White;
+                label7.BackColor = Color.White;
+                label8.BackColor = Color.White;
+                label9.BackColor = Color.White;
+                label10.BackColor = Color.White;
+                label11.BackColor = Color.White;
+                label12.BackColor = Color.White;
                 x++;
             }
             else if (x == 1)
             {
                 label2.BackColor = Color.Red;
+                label1.BackColor = Color.White;
+                label3.BackColor = Color.White;
+                label4.BackColor = Color.White;
+                label5.BackColor = Color.White;
+                label6.BackColor = Color.White;
+                label7.BackColor = Color.White;
+                label8.BackColor = Color.White;
+                label9.BackColor = Color.White;
+                label10.BackColor = Color.White;
+                label11.BackColor = Color.White;
+                label12.BackColor = Color.White;
                 x++;
             }
+            else if (x == 2)
+            {
+                label3.BackColor = Color.Red;
+                label2.BackColor = Color.White;
+                label1.BackColor = Color.White;
+                label4.BackColor = Color.White;
+                label5.BackColor = Color.White;
+                label6.BackColor = Color.White;
+                label7.BackColor = Color.White;
+                label8.BackColor = Color.White;
+                label9.BackColor = Color.White;
+                label10.BackColor = Color.White;
+                label11.BackColor = Color.White;
+                label12.BackColor = Color.White;
+                x++;
+            }
+            else if (x == 3)
+            {
+                label4.BackColor = Color.Red;
+                label2.BackColor = Color.White;
+                label3.BackColor = Color.White;
+                label1.BackColor = Color.White;
+                label5.BackColor = Color.White;
+                label6.BackColor = Color.White;
+                label7.BackColor = Color.White;
+                label8.BackColor = Color.White;
+                label9.BackColor = Color.White;
+                label10.BackColor = Color.White;
+                label11.BackColor = Color.White;
+                label12.BackColor = Color.White;
+                x++;
+            }
+            else if (x == 4)
+            {
+                label5.BackColor = Color.Red;
+                label2.BackColor = Color.White;
+                label3.BackColor = Color.White;
+                label4.BackColor = Color.White;
+                label1.BackColor = Color.White;
+                label6.BackColor = Color.White;
+                label7.BackColor = Color.White;
+                label8.BackColor = Color.White;
+                label9.BackColor = Color.White;
+                label10.BackColor = Color.White;
+                label11.BackColor = Color.White;
+                label12.BackColor = Color.White;
+                x++;
+            }
+            else if (x == 5)
+            {
+                label6.BackColor = Color.Red;
+                label2.BackColor = Color.White;
+                label3.BackColor = Color.White;
+                label4.BackColor = Color.White;
+                label5.BackColor = Color.White;
+                label1.BackColor = Color.White;
+                label7.BackColor = Color.White;
+                label8.BackColor = Color.White;
+                label9.BackColor = Color.White;
+                label10.BackColor = Color.White;
+                label11.BackColor = Color.White;
+                label12.BackColor = Color.White;
+                x++;
+            }
+            else if (x == 6)
+            {
+                label7.BackColor = Color.Red;
+                label2.BackColor = Color.White;
+                label3.BackColor = Color.White;
+                label4.BackColor = Color.White;
+                label5.BackColor = Color.White;
+                label6.BackColor = Color.White;
+                label1.BackColor = Color.White;
+                label8.BackColor = Color.White;
+                label9.BackColor = Color.White;
+                label10.BackColor = Color.White;
+                label11.BackColor = Color.White;
+                label12.BackColor = Color.White;
+                x++;
+            }
+            else if (x == 7)
+            {
+                label8.BackColor = Color.Red;
+                label2.BackColor = Color.White;
+                label3.BackColor = Color.White;
+                label4.BackColor = Color.White;
+                label5.BackColor = Color.White;
+                label6.BackColor = Color.White;
+                label7.BackColor = Color.White;
+                label1.BackColor = Color.White;
+                label9.BackColor = Color.White;
+                label10.BackColor = Color.White;
+                label11.BackColor = Color.White;
+                label12.BackColor = Color.White;
+                x++;
+            }
+            else if (x == 8)
+            {
+                label9.BackColor = Color.Red;
+                label2.BackColor = Color.White;
+                label3.BackColor = Color.White;
+                label4.BackColor = Color.White;
+                label5.BackColor = Color.White;
+                label6.BackColor = Color.White;
+                label7.BackColor = Color.White;
+                label8.BackColor = Color.White;
+                label1.BackColor = Color.White;
+                label10.BackColor = Color.White;
+                label11.BackColor = Color.White;
+                label12.BackColor = Color.White;
+                x++;
+            }
+            else if (x == 9)
+            {
+                label10.BackColor = Color.Red;
+                label2.BackColor = Color.White;
+                label3.BackColor = Color.White;
+                label4.BackColor = Color.White;
+                label5.BackColor = Color.White;
+                label6.BackColor = Color.White;
+                label7.BackColor = Color.White;
+                label8.BackColor = Color.White;
+                label9.BackColor = Color.White;
+                label1.BackColor = Color.White;
+                label11.BackColor = Color.White;
+                label12.BackColor = Color.White;
+                x++;
+            }
+            else if (x == 10)
+            {
+                label11.BackColor = Color.Red;
+                label2.BackColor = Color.White;
+                label3.BackColor = Color.White;
+                label4.BackColor = Color.White;
+                label5.BackColor = Color.White;
+                label6.BackColor = Color.White;
+                label7.BackColor = Color.White;
+                label8.BackColor = Color.White;
+                label9.BackColor = Color.White;
+                label10.BackColor = Color.White;
+                label1.BackColor = Color.White;
+                label12.BackColor = Color.White;
+                x++;
+            }
+            else if (x == 11)
+            {
+                label12.BackColor = Color.Red;
+                label2.BackColor = Color.White;
+                label3.BackColor = Color.White;
+                label4.BackColor = Color.White;
+                label5.BackColor = Color.White;
+                label6.BackColor = Color.White;
+                label7.BackColor = Color.White;
+                label8.BackColor = Color.White;
+                label9.BackColor = Color.White;
+                label10.BackColor = Color.White;
+                label11.BackColor = Color.White;
+                label1.BackColor = Color.White;
+                x++;
+            }
+            x %= 12;
+
         }
 
         private void pictureBox1_Paint(object sender, PaintEventArgs e)
@@ -47,6 +231,16 @@ namespace Final3
         }
 
         private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
         {
 
         }
