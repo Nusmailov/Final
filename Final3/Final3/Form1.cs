861879d7a2f2feceb4aded644ce73a66499f964f
Nurzhigit <nusmailov@gmail.com>
2017-05-20 11:00:07 +0600

    11:00

--- Final3/Final3/Form1.cs
+++ Final3/Final3/Form1.cs
@@ -19,7 +19,36 @@ namespace Final3
         int c = 12;
         private void pictureBox1_Click(object sender, EventArgs e)
         {
-            
+
+        }
+        int x = 0;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (x == 0)
+            {
+                label1.BackColor = Color.Red;
+                x++;
+            }
+            else if (x == 1)
+            {
+                label2.BackColor = Color.Red;
+                x++;
+            }
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
